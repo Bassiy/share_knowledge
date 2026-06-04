a38b130da8925375de638bf449f204f2a9e874e5
Bassiy <yu4.to6@gmail.com>
2026-06-04 14:39:19 +0000

    sync: update from info repo

--- coding/csharp_interface/app/obj/Debug/net10.0/app.AssemblyInfo.cs
+++ coding/csharp_interface/app/obj/Debug/net10.0/app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208ee64f0cd0c914149f713815565bd3daf400ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f700d664593a9efe4752b2071bdb10324ed2c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
